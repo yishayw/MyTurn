5e2db069a4aa7ddf55b2d375d58b3f84384eb268
yishayw <yishayjobs@hotmail.com>
2012-10-24 11:42:58 +0300

    Native Android support

--- public/app/store/Groups.js
+++ public/app/store/Groups.js
@@ -1,10 +1,11 @@
 Ext.define('testing.store.Groups', {
     extend: 'Ext.data.Store',
+    requires: ['testing.proxy.CrossAjax'],
     config: {
         model: 'testing.model.Group',
         storeId: 'groups',
         proxy: {
-            type: 'ajax',
+            type: 'crossajax',
             cacheString: 'dcdcdc',
             url : 'api/data/groups.json',
             api: {
