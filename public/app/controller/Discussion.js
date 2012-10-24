b4b6e88e838d838a4550b3e689973143808994e5
yishayw <yishayjobs@hotmail.com>
2012-10-24 11:26:24 +0300

    Native Android support

--- public/app/controller/Discussion.js
+++ public/app/controller/Discussion.js
@@ -1,70 +1,8 @@
-Ext.define('testing.controller.Discussion', {
-    extend: 'Ext.app.Controller',
-    requires: ['testing.util.TimeUtils'],
-    config: {
-        refs: {
-            addToQueueButton: "button[action=addToQueueEvent]",
-            messageLabel: "#messageLabel",
-            timeRemainingLabel: "#timeRemainingLabel",
-            beepSound: "#beeper",
-            tickSound: "#ticker"
-        }
-    },
 
-    doAddToQueue: function () {
-        this.getApplication().fireEvent('clientMessage', { type: 'requestToSpeak' });
-    },
-
-    doRemoveFromQueue: function () {
-        this.getApplication().fireEvent('clientMessage', { type: 'relinquishTurn' });
-    },
-
-    doDiscussionOver: function (data) {
-        Ext.Msg.alert('', 'The discussion is over.');
-        // a group was deleted on server, time to reload
-        Ext.getStore('groups').load();
-    },
-
-    doUsersSaved: function(data) {
-       this.clearTick();
-       this.initMessageScreen();
-    },
-
-    doNewSpeaker: function (data) {
-        this.getMessageLabel().setHtml('Current speaker is ' + data.name);
-        this.doUpdateTimeRemaining(data);
-        if (this.getUserName() != data.name) {
-            this.clearTick();
-        }
-    },
-
-    doWaitingForNewSpeaker: function (data) {
-        this.getMessageLabel().setHtml('Waiting for New Speaker');
-        this.doUpdateTimeRemaining(data);
-        this.getBeepSound().play();
-        this.clearTick();
-    },
-
-    getUserName: function () {
-        var users = Ext.getStore('defaultUsers');
-        if (users.getCount() == 1) {
-            return users.getAt(0).get('name');
-        }
-        return null;
-    },
-
-    initMessageScreen: function () {
-        this.getMessageLabel().setHtml('Waiting for New Speaker');
-        this.getTimeRemainingLabel().setHtml('');
-    },
-
-    clearTick: function () {
-        if (this.tickSoundInterval) {
-            clearInterval(this.tickSound
 Ext.define('testing.controller.Discussion', {
     extend: 'Ext.app.Controller',
     requires: [
-        'testing.util.UrlUtils',
+    	'testing.util.UrlUtils',
     	'testing.util.TimeUtils', 
     	'Ext.device.Notification'
     ],
@@ -228,50 +166,5 @@ Ext.define('testing.controller.Discussion', {
     	);
     	this.setNativeTickSound(tickMedia);
     	this.setNativeBeepSound(beepMedia);
-    }
-});Interval);
-            this.tickSoundInterval = null;
-        }
-    },
-
-    doMyTurn: function (data) {
-        if (this.tickSoundInterval) {
-            return;
-        }
-        this.getBeepSound().play();
-        var context = this;
-        this.clearTick();
-        this.tickSoundInterval = setInterval(function () {
-            context.doTick();
-        }, 1000);
-    },
-
-    doTick: function () {
-        this.getTickSound().play();
-    },
-
-    doUpdateTimeRemaining: function (data) {
-        var formattedTime = testing.util.TimeUtils.getFormattedTime(data.timeLeft);
-        this.getTimeRemainingLabel().setHtml(formattedTime);
-    },
-
-    init: function () {
-        this.getApplication().on({
-            discussionOver: this.doDiscussionOver,
-            usersSaved: this.doUsersSaved,
-            newSpeaker: this.doNewSpeaker,
-            yourTurn: this.doMyTurn,
-            waitingForNewSpeaker: this.doWaitingForNewSpeaker,
-            scope: this
-        });
-    },
-
-    launch: function () {
-        this.getAddToQueueButton().element.on({
-            touchstart: 'doAddToQueue',
-            touchend: 'doRemoveFromQueue',
-            scope: this
-        });
-        this.initMessageScreen();
     }
 });
